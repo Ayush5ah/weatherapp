e00c9c2ba8779a87cc536287f13eac6280b4d856
Ayush Kumar Sah <97509201+Ayush5ah@users.noreply.github.com>
2024-07-17 17:01:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-
+const url = 'https://weather-api-by-any-city.p.rapidapi.com/weather/London';
 const options = {
 	method: 'GET',
 	headers: {
 		'x-rapidapi-key': 'f152517b04msh301d9c32dfd22f1p16c09ejsnbffa23402277',
-		'x-rapidapi-host': 'weather-by-api-ninjas.p.rapidapi.com'
+		'x-rapidapi-host': 'weather-api-by-any-city.p.rapidapi.com'
 	}
 };
 
@@ -14,25 +14,24 @@ try {
 } catch (error) {
 	console.error(error);
 }
-
 const getWeather=(city)=>{
     cityName.innerHTML=city
-    fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city='+city, options)
+    fetch('https://weather-api-by-any-city.p.rapidapi.com/weather/?city='+city, options)
     .then(response => response.json())
     .then(response => {
         console.log(response)
-        temp.innerHTML = response.temp
-        temp2.innerHTML = response.temp
-        feels_like.innerHTML = response.feels_like
+        temp.innerHTML = response.temp_c
+        temp2.innerHTML = response.temp_c
+        feels_like.innerHTML = response.feelslike_c
         humidity.innerHTML = response.humidity
         humidity2.innerHTML = response.humidity
-        min_temp.innerHTML = response.min_temp
-        max_temp.innerHTML = response.max_temp
-        wind_speed.innerHTML = response.wind_speed
-        wind_speed2.innerHTML = response.wind_speed
+        min_temp.innerHTML = response.windchill_c
+        max_temp.innerHTML = response.heatindex_c
+        wind_speed.innerHTML = response.wind_kph
+        wind_speed2.innerHTML = response.wind_kph
         wind_degrees.innerHTML = response.wind_degrees
-        sunrise.innerHTML = response.sunrise
-        sunset.innerHTML = response.sunset
+        sunrise.innerHTML = response.is_day
+        sunset.innerHTML = response.is_day
     } )
 
     .catch(err => console.error(err));
